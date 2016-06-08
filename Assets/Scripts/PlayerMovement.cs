b0b020b3ffb58f37caa2f94f16fa3bff617d596c
Dayna Lacoursiere <Dayna Lacoursiere>
2016-06-07 18:24:50 -0600

    pc follows the mouse around the screen

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Vector2 mousePosition;
+	public float moveSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+	}
+}
