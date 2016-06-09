5c1fab3b87b79daee1a559dd7340761d22b7c67c
Dayna Lacoursiere <Dayna Lacoursiere>
2016-06-08 21:02:53 -0600

    Changed to background movement and started working on piece movement

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class BackgroundMovement : MonoBehaviour {
 
 	private Vector2 mousePosition;
-	public float moveSpeed = 0.1f;
+	private Vector2 newPosition;
+	public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-		transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+		newPosition = transform.position;
+		newPosition.x = newPosition.x - mousePosition.x;
+		newPosition.y = newPosition.y - mousePosition.y;
+		transform.position = Vector2.Lerp (transform.position, newPosition, moveSpeed);
+
 	}
 }
---
+++ Assets/Scripts/PiecesMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PiecesMovement : MonoBehaviour {
+
+	private Vector2 randomspot;
+	public float speed;
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer = timer - Time.deltaTime;
+		if (timer < 1) {
+			timer = 15;
+		}
+		randomspot = new Vector2 (Random.Range (5, 50), Random.Range (5, 50));
+		transform.position = Vector2.Lerp (transform.position, randomspot, speed);
+	}
+}
