f7f3cb49e4d3c166f577876d80abaf7f9e7bf7ae
Dayna Lacoursiere <Dayna Lacoursiere>
2016-06-12 21:38:38 -0600

    Got the other pieces moving randomly

--- Assets/Scripts/PiecesMovement.cs
+++ Assets/Scripts/PiecesMovement.cs
@@ -3,22 +3,32 @@ using System.Collections;
 
 public class PiecesMovement : MonoBehaviour {
 
-	private Vector2 randomspot;
-	public float speed;
-	public float timer;
+	public float speed = 0.001f;
+	public float counter = 10;
+	private float counter2 = 10;
+	public Vector2 endpoint;
+	private int numberOfLoops = 1;
 
 	// Use this for initialization
 	void Start () {
-
+		RandomPostion ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		timer = timer - Time.deltaTime;
-		if (timer < 1) {
-			timer = 15;
+		counter2 -= Time.deltaTime;
+		if (counter2 < 0 && numberOfLoops > 0) {
+			RandomPostion ();
+			numberOfLoops--;
+			counter2 = counter;
 		}
-		randomspot = new Vector2 (Random.Range (5, 50), Random.Range (5, 50));
-		transform.position = Vector2.Lerp (transform.position, randomspot, speed);
+
+		transform.position = Vector2.Lerp (transform.position, endpoint, speed);
+	}
+
+	void RandomPostion () {
+		float x = Random.Range (-50f, 50f);
+		float y = Random.Range (-100f, 100f);
+		endpoint = new Vector2 (x, y);
 	}
 }
